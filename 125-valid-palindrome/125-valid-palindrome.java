ff4c40ed8288c0958522b89d27aa3db0d9f419d0
Suraj Singh <84654095+suraj-singh127@users.noreply.github.com>
2022-06-03 15:25:12 +0530

    Time: 7 ms (52.04%), Space: 43.2 MB (75.90%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,19 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder result = new StringBuilder();
-        for(int i = 0;i<s.length();i++){
-            int code = (int)(s.charAt(i));
-            if(code>=65 && code<=90){
-                code+=32;
-                result.append((char)(code));
+        int start = 0; int end = s.length() - 1;
+        while(start<end){
+            while(start<=end && !Character.isLetterOrDigit(s.charAt(start))){
+                start++;
             }
-            else if((code>=97 && code<=122) || (code>=48 && code<=57)){
-                result.append((char)(code));
+            while(start<=end && !Character.isLetterOrDigit(s.charAt(end))){
+                end--;
             }
-        }
-        System.out.println(result);
-        for(int i = 0,j = result.length()-1;i<=j;i++,j--){
-            if(result.charAt(i)!=result.charAt(j)) return false;
+            if(start<=end && Character.toLowerCase(s.charAt(start++))!=Character.toLowerCase(s.charAt(end--))){
+                return false;
+            }
         }
         return true;
     }
